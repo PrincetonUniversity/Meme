8f91cd7f77f901424a5fa852d435bbfcbe460f09
robertmacdavid <robertmacdavid@gmail.com>
2016-07-21 12:21:45 -0400

    initial commit

---
+++ evaluation/partialOrdering.py
@@ -0,0 +1,18 @@
+
+
+
+def inOrder(x, y):
+
+
+
+path1 = [1,2,3,4]
+
+order2 = [1,3,2,4]
+
+sets = [order1, set2]
+
+occurrences = {}
+
+for 
+
+
---
+++ writing/README.md
@@ -0,0 +1,4 @@
+dpal
+========
+
+Flowspace Authorization Project
