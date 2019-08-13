051c765149428b55b72b921b34a8a0480d4219f4
robertmacdavid <robertmacdavid@gmail.com>
2019-08-13 11:15:26 -0400

    more code cleaning

--- BaseCodes.py
+++ BaseCodes.py
@@ -40,16 +40,11 @@ class BaseCode(ABC):
         self.logger = logging.getLogger(type(self).__name__)
 
     @abstractmethod
-    def make(self, **kwargs) -> None:
+    def make(self, *args, **kwargs) -> None:
         """ Build the code.
         """
         pass
 
-    @abstractmethod
-    def width(self, includePadding : bool = True) -> int:
-        """ Number of bits necessary for this code.
-        """
-        pass
 
     @abstractmethod
     def tag(self, row : Row, decorated : bool = False) -> BinaryString:
@@ -65,10 +60,20 @@ class BaseCode(ABC):
         pass
 
 
+    def width(self, includePadding : bool = True) -> int:
+        """ Number of bits necessary for this code.
+        """
+        self.logger.debug("Placeholder width method of base code class was called. Override recommended.")
+        longestTag = max(len(tag) for tag in self.allTags().values())
+        longestQuery = max(max(len(qStr) for qStr in qStrs) for qStrs in self.allMatchStrings().values())
+        return max(longestTag, longestQuery)
+
+
     def allTags(self, decorated : bool = False) -> Dict[FixedRow, BinaryString]:
         """ Returns a dictionary mapping matrix rows to compressed tags.
         """
         self.logger.debug("allTags of base code class was called.")
+        assert self.made
         return {row : self.tag(row=row, decorated=decorated) for row in self.originalRows}
 
 
@@ -77,22 +82,24 @@ class BaseCode(ABC):
             when compared to a row's tag, will compare True if the columnID is present in the tag.
         """
         self.logger.debug("allMatchStrings of base code class was called.")
+        assert self.made
         return {colID : self.matchStrings(colID, decorated=decorated) for colID in self.columnIDs}
 
 
-    def timeMake(self, **kwargs) -> float:
-        """ Run self.make(**kwargs), and print how long it took.
+    def timeMake(self, *args, **kwargs) -> float:
+        """ Run self.make(*args, **kwargs), and print how long it took.
         """
         className = type(self).__name__
         numRows = len(self.originalRows)
         numCols = len(self.columnIDs)
         print("Beginning code make for %4d rows and %4d columns using code class %s." % (numRows, numCols, className))
         startTime = time.time()
-        self.make(**kwargs)
+        self.make(*args, **kwargs)
         endTime = time.time()
 
         elapsed = endTime - startTime
         print("Make done. Took %4.2f seconds." % elapsed)
+        assert self.made # make sure the make method properly sets self.made
         return elapsed
 
 
@@ -171,8 +178,8 @@ class NaiveCode(BaseCodeStatic):
     def make(self):
         self.made = True
 
-    def width(self, includePadding=False):
-        return len(self.columnIDs)
+    #def width(self, includePadding=False):
+    #    return len(self.columnIDs)
 
     def tag(self, row, decorated=False):
         return ''.join(['1' if colID in row else '0' for colID in self.columnIDs])
---
+++ ClusterCode.py
@@ -0,0 +1,202 @@
+from BaseCodes import BaseCodeStatic, ColumnID, Row, Matrix, BinaryString
+from common import generateIdentifiers, kraftsInequality
+from optimize import removeSubsets, minimizeRulesGreedy, minimizeVariableWidthGreedy
+
+from typing import List, Set, Dict, FrozenSet
+
+# memory requirements are
+#   sum( sum(tagWidth + overhead for overhead in ruleOverheads[elem])*occurrences[elem] for elem in elements)
+
+# compare the minimum possible tag size that the schemes can achieve
+# then, compare the minimum rule inflation the schemes achieve below different field widths: 16, 32, 48, 64 bits
+
+
+class ClusterCode(BaseCodeStatic):
+
+    clustersMade : bool = False
+    clusters : Matrix = None
+    identifiersMade : bool = False
+    identifiers : List[BinaryString] = None
+    freeIdentifiers : List[BinaryString] = None
+
+    hideImplicits : bool = True
+    clusterImplicits : List[Set[ColumnID]] = None
+
+    def __init__(self, matrix=None, hideImplicits=True, **kwargs):
+        super().__init__(matrix=matrix, **kwargs)
+
+        self.clusters = []
+        self.identfiers = []
+        self.hideImplicits = hideImplicits
+        if self.hideImplicits:
+            self.clusterImplicits = []
+
+
+
+    def makeClusters(self, rowGroupings : List[List[Row]]):
+
+        rowGroupings = [[set(row) for row in group] for group in rowGroupings]
+
+        clusters = [set.union(*group) for group in rowGroupings]
+        implicits = []
+        if self.hideImplicits:
+            implicits = [set.intersection(*group) for group in rowGroupings]
+
+        self.setClusters(clusters, implicits)
+
+
+    def setClusters(self, clusters, implicits=None):
+        self.made = False
+        self.clusters = [set(cluster) for cluster in clusters]
+        if implicits == None or not self.hideImplicits:
+            implicits = [[] for _ in self.clusters]
+        else:
+            self.clusterImplicits = list(implicits)
+        self.clustersMade = True
+
+
+
+    def makeIdentifiers(self, identifiers=None):
+        self.made = False
+        if identifiers != None:
+            if len(identifiers) != len(self.clusters):
+                raise exception("Given set of identifiers is not same length as set of clusters!")
+            self.identifiers = list(identifiers)
+            return
+
+        idLengths = [len(cluster) for cluster in self.clusters]
+        if self.hideImplicits:
+            idLengths = [idLen - len(implicits) for idLen, implicits in zip(idLengths, self.clusterImplicits)]
+        self.identifiers, self.freeIdentifiers = generateIdentifiers(idLengths)
+
+        self.identifiersMade = True
+
+
+
+    def make(self, *args, **kwargs):
+        assert self.clustersMade and self.identifiersMade
+
+        self.made = True
+
+
+
+    def width(self):
+        if not self.made:
+            return -1
+        return max(len(cluster) + len(ID) for cluster,ID in zip(self.clusters, self.identifiers))
+
+
+
+    def indexOfCluster(self, subset):
+        for i, cluster in enumerate(self.clusters):
+            if cluster.issuperset(subset):
+                return i
+        return -1
+
+
+
+    def buildString(self, ssIndex, mask=None, subset=None, padChar='0', maskNegChar='', partChar='', width=None):
+        """ padChar can be '0' or '*'
+            maskNegChar is the characters for columnIDs absent from a mask. Can be '0' or '*'
+            partChar can be '' or '|', and goes between the identifier and padding, and the padding and mask
+        """
+        assert not (mask == None and subset == None)
+        if maskNegChar == '':
+            maskNegChar = padChar
+        if width == None:
+            width = self.width()
+
+
+        identifier = self.identifiers[ssIndex]
+
+        cluster = self.clusters[ssIndex]
+        if self.hideImplicits:
+            cluster = cluster.difference_update(self.clusterImplicits[ssIndex])
+
+
+        if mask == None:
+            mask = ''.join(['1' if colID in subset else maskNegChar for colID in cluster])
+
+        padding = padChar * (width - (len(identifier) + len(mask)))
+
+        return identifier + partChar + padding + partChar + mask
+
+
+
+    def tag(self, row, decorated=False):
+        assert self.made
+        index = self.indexOfCluster(row)
+
+        partChar = '|' if decorated else ''
+        return self.buildString(index, subset=row, padChar='0', partChar=partChar)
+
+
+    def matchStrings(self, columnID, decorated=False):
+        assert self.made
+        tagWidth = self.width()
+        partChar = '|' if decorated else ''
+        outStrings = []
+        for i, superset in enumerate(self.supersets):
+            if columnID in superset:
+                outStrings.append(self.buildString(i, subset=[columnID], width=tagWidth, padChar='*', partChar=partChar))
+
+        return outStrings
+
+
+    def allMatchStrings(self, decorated=False):
+        assert self.made
+        tagWidth = self.width()
+        partChar = '|' if decorated else ''
+
+        outStrings = {colID : [] for colID in self.columnIDs}
+        for cID, cluster in enumerate(self.clusters):
+            maskBits = ['*'] * len(cluster)
+            for i, columnID in enumerate(cluster):
+                maskBits[i] = '1'
+                mask = ''.join(maskBits)
+                outStrings[columnID].append(self.buildString(cID, mask=mask, width=tagWidth, padChar='*', partChar=partChar))
+                maskBits[i] = '*'
+
+        return outStrings
+
+
+class OriginalCodeStatic(ClusterCode):
+
+    def __init__(self, matrix=None, **kwargs):
+        print(matrix)
+        super().__init__(matrix=matrix, hideImplicits=False, **kwargs)
+
+
+    def make(self, optWidth=True, maxWidth=-1):
+        supersets = removeSubsets(self.originalRows)
+
+        if optWidth:
+            supersets = minimizeVariableWidthGreedy(supersets)
+
+        minWidth = kraftsInequality([len(superset) for superset in supersets])
+        if maxWidth != -1:
+            if maxWidth >= minWidth:
+                supersets = minimizeRulesGreedy(supersets, {colID:1 for colID in self.columnIDs}, maxWidth)
+            else:
+                self.logger.warning("Given maximum tag width is too small! Max given is %d, min is %d" % (maxWidth, minWidth))
+
+        self.setClusters(supersets)
+
+        self.makeIdentifiers()
+
+        self.made = True
+
+
+
+
+def main():
+    matrix = [[1,2,3], [3,4,5], [6,7]]
+    code = OriginalCodeStatic(matrix=matrix)
+    code.timeMake(maxWidth=4, optWidth=True)
+    code.verifyCompression()
+    print("Memory of original code:", code.memoryRequired())
+
+
+
+if __name__ == "__main__":
+    main()
